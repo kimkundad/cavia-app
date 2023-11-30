3144fd01c399def44836cfae79d147db282d640d
kimkundad <kim.kundad@gmail.com>
2023-11-30 15:51:53 +0700

    kim

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -159,7 +159,7 @@ window.gaTitle = 'หน้าแรก';
                       @if(isset($point))
                       @foreach($point as $u)
                         <tr>
-                          <td>{{ ($u->date) }}</td>
+                          <td>{{ ($u->created_at) }}</td>
                           <td>@if($u->type == 1)
                                                         {{ $u->detail }}
                                                         @elseif($u->type == 2)
