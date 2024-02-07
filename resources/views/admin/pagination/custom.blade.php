b42ff23a574f2ca343cb20eebf4ffba796dcbe44
kimkundad <kim.kundad@gmail.com>
2024-02-08 01:29:18 +0700

    ki

--- resources/views/admin/pagination/custom.blade.php
+++ resources/views/admin/pagination/custom.blade.php
@@ -1,28 +1,28 @@
 @if ($paginator->hasPages())
-<ul class="pager">
+<nav>
     @if ($paginator->onFirstPage())
-        <li class="disabled"><span>← Previous</span></li>
+        <li class="page-item disabled"><span><i class="icon-arrow-left"></i> Previous</span></li>
     @else
-        <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">← Previous</a></li>
+        <li class="page-item"><a href="{{ $paginator->previousPageUrl() }}" rel="prev">← Previous</a></li>
     @endif
     @foreach ($elements as $element)
         @if (is_string($element))
-            <li class="disabled"><span>{{ $element }}</span></li>
+            <li class="page-item disabled"><span>{{ $element }}</span></li>
         @endif
         @if (is_array($element))
             @foreach ($element as $page => $url)
                 @if ($page == $paginator->currentPage())
-                    <li class="active my-active"><span>{{ $page }}</span></li>
+                    <li class="page-item active my-active"><span>{{ $page }}</span></li>
                 @else
-                    <li><a href="{{ $url }}">{{ $page }}</a></li>
+                    <li class="page-item"><a href="{{ $url }}">{{ $page }}</a></li>
                 @endif
             @endforeach
         @endif
     @endforeach
     @if ($paginator->hasMorePages())
-        <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next →</a></li>
+        <li class="page-item"><a href="{{ $paginator->nextPageUrl() }}" rel="next">Next <i class="icon-arrow-right"></i></a></li>
     @else
-        <li class="disabled"><span>Next →</span></li>
+        <li class="page-item disabled"><span>Next <i class="icon-arrow-right"></i></span></li>
     @endif
-</ul>
+</nav>
 @endif 
